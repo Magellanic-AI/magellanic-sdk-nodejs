2b076d1deaed2033cfb1ec97d8585405eb22c404
Paweł <pawel.krzyzak@almine.pl>
2023-07-19 12:58:21 +0200

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'import'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+    'plugin:import/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['.eslintrc.js', '**/dist/'],
+  rules: {
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-restricted-imports': 'off',
+    '@typescript-eslint/no-restricted-imports': [
+      'error',
+      {
+        patterns: ['src'],
+      },
+    ],
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts'],
+    },
+    'import/resolver': {
+      node: true,
+      typescript: true,
+    },
+  },
+};
---
+++ src/magellanic.client.ts
@@ -0,0 +1,27 @@
+import { v4 as uuid } from 'uuid';
+import axios, { AxiosInstance } from 'axios';
+
+const API_URL = 'https://api.magellanic.one';
+
+export class MagellanicClient {
+  private readonly randomInitString: string;
+  private readonly axiosInstance: AxiosInstance;
+
+  constructor(private readonly tdtiId: string) {
+    this.randomInitString = uuid();
+    this.axiosInstance = axios.create({
+      baseURL: API_URL,
+      headers: {
+        'magellanic-tdti-id': tdtiId,
+      },
+    });
+  }
+
+  async authenticate() {
+    await axios.post(`workloads`, { randomString: this.randomInitString });
+  }
+
+  validateAuthWebhookPayload(payload: Record<string, unknown>): boolean {
+    return payload.randomString === this.randomInitString;
+  }
+}
