4700fb0be5c2eb3f01e5071081ceab797a37ca40
Paweł <pawel.krzyzak@almine.pl>
2023-07-21 13:56:17 +0200

    update: handle webhooks

---
+++ src/helpers.ts
@@ -0,0 +1,34 @@
+import crypto from 'crypto';
+
+const GCM_ALGORITHM = 'aes-256-gcm';
+const GCM_IV_LENGTH = 12;
+const GCM_TAG_LENGTH = 16;
+
+export function encryptAes(data: string, key: string) {
+  const iv = crypto.randomBytes(GCM_IV_LENGTH);
+  const cipher = crypto.createCipheriv(GCM_ALGORITHM, key, iv);
+  return Buffer.concat([
+    iv,
+    cipher.update(data, 'utf8'),
+    cipher.final(),
+    cipher.getAuthTag(),
+  ]).toString('base64');
+}
+
+export function decryptAes(encryptedData: string, key: string) {
+  const message = Buffer.from(encryptedData, 'base64');
+  const iv = Buffer.allocUnsafe(GCM_IV_LENGTH);
+  const tag = Buffer.allocUnsafe(GCM_TAG_LENGTH);
+  const data = Buffer.alloc(message.length - GCM_IV_LENGTH - GCM_TAG_LENGTH, 0);
+
+  message.copy(iv, 0, 0, GCM_IV_LENGTH);
+  message.copy(tag, 0, message.length - GCM_TAG_LENGTH);
+  message.copy(data, 0, GCM_IV_LENGTH);
+
+  const decipher = crypto.createDecipheriv(GCM_ALGORITHM, key, iv);
+  decipher.setAuthTag(tag);
+
+  let decrypted = decipher.update(data, undefined, 'utf8');
+  decrypted += decipher.final('utf8');
+  return decrypted;
+}
--- src/magellanic.client.ts
+++ src/magellanic.client.ts
@@ -1,14 +1,24 @@
 import { v4 as uuid } from 'uuid';
 import axios, { AxiosInstance } from 'axios';
+import { decryptAes } from './helpers';
 
 const API_URL = 'https://api.magellanic.one/public-api/workloads';
 
 export class MagellanicClient {
-  private readonly randomInitString: string;
   private readonly axiosInstance: AxiosInstance;
+  private authRandomString: string;
+
+  private currentTokensSecret?: string;
+  private previousTokensSecret?: string;
+
+  private secret?: string;
+  private currentTokens?: Record<string, string>;
+  private previousTokens?: Record<string, string>;
+  private currentToken?: string;
+  private previousToken?: string;
 
   constructor(private readonly tdtiId: string) {
-    this.randomInitString = uuid();
+    this.authRandomString = uuid();
     this.axiosInstance = axios.create({
       baseURL: API_URL,
       headers: {
@@ -17,17 +27,60 @@ export class MagellanicClient {
     });
   }
 
-  async authenticate() {
+  async authenticate(reauth = false) {
+    if (reauth) {
+      this.authRandomString = uuid();
+    }
     await this.axiosInstance.post(`auth`, {
-      randomString: this.randomInitString,
+      randomString: this.authRandomString,
     });
   }
 
-  validateAuthWebhookPayload(payload: any): {
-    result: boolean;
-  } {
-    return {
-      result: payload && payload.randomString === this.randomInitString,
-    };
+  //TODO: errors handling
+  async handleWebhook(payload: any): Promise<boolean> {
+    if (!payload.action) {
+      return false;
+    }
+    switch (payload.action) {
+      case 'auth': {
+        this.secret = payload.secret;
+        return payload && payload.randomString === this.authRandomString;
+      }
+      case 'init': {
+        const data = this.decryptPayload(payload);
+        this.secret = data.secret;
+        if (data.state) {
+          this.currentTokensSecret = data.state.tokensSecret;
+          this.currentTokens = data.state.tokens;
+        }
+        break;
+      }
+      case 'rotateTokens': {
+        const data = this.decryptPayload(payload);
+        this.secret = data.secret;
+        this.previousTokensSecret = this.currentTokensSecret;
+        this.currentTokensSecret = data.tokensSecret;
+        this.previousTokens = this.currentTokens;
+        this.currentTokens = data.tokens;
+        this.previousToken = this.currentToken;
+        this.currentToken = data.myToken;
+        break;
+      }
+      case 'reauth': {
+        await this.authenticate(true);
+        break;
+      }
+      default:
+        return false;
+    }
+    return true;
+  }
+
+  private decryptPayload(payload: any) {
+    if (!this.secret) {
+      throw new Error('secret not set');
+    }
+    const decrypted = decryptAes(payload.encrypted, this.secret);
+    return JSON.parse(decrypted);
   }
 }
